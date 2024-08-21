274a40af84e9046c8acfe01156dfe9bf309843bd
Akhilesh Maurya <Akhilesh.Maurya@godigit.com>
2024-08-21 11:16:08 +0530

    This is Engine class

---
+++ main/java/com/example/Day_21/Engin_wxml.java
@@ -0,0 +1,13 @@
+package com.example.Day_21;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Engin_wxml {
+  public void new_engin(){
+    System.out.println("Working on engine");
+  }
+}
+
+
+
